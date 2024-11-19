fce2765cb86fab319d3c3896d065a341ff63fe5c
Mihir-21 <trambadiyamihir635@gmail.com>
2024-11-19 19:05:41 +0530

    Update Program.cs

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Do what are you doing ..");
+app.MapGet("/", () => "Do it Smartly");
 
 app.Run();
